da6c319753b02533f0aa10dc69f91d891f8dc558
Robby Kim <robert.st.kim@gmail.com>
2016-06-15 15:53:03 -0400

    finished basic gameplay

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){	
-	var randomNum = enerateRandomNum();
+	var randomNum = generateRandomNum();
+  var guessCount = 0;
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -15,42 +16,62 @@ $(document).ready(function(){
   	//Event listener for user input
   	$("form").submit(function() {
   		event.preventDefault(); //Stops page from reloading
-  		if(/*Not a number*/) 
-  		getUserInput();
+  		var userGuess = parseInt($("#userGuess").val());
+      // determines if input is a number
+      // if so, runs the comparison
+      if (!(isNaN(userGuess))) {
+        compareNum(userGuess, randomNum);
+        guessList(userGuess);
+        guessCount++;
+        $('#count').text(guessCount);
+      } else {
+        $("#userGuess").val("");
+        alert("Please enter a number.");
+      }
+  		
   	})
 
 });
 
-//function tracks the state of the game
-function game() {
-	//Track how many guesses
-	//Extra: Keep track of guesses	
+// Compares numbers and gives feedback for each guess
+function compareNum(user, random) {
+  var difference = Math.abs(user - random);
+  console.log(difference);
+  if (difference >= 50) {
+    $('#feedback').html('Ice Cold!');
+    console.log('50');
+  } else if (difference >= 30) {
+    $('#feedback').html('Cold');
+    console.log('30');
+  } else if (difference >= 10) {
+    $('#feedback').html('Warm');
+    console.log('10');
+  } else if (difference >= 1) {
+    $('#feedback').html('Very HOT!');
+  } else {
+    $('#feedback').html('You got it!');
+  }
 }
 
 //Function to restart everythin (newGame) 
 function newGame() {
-
+  guessCount = 0;
+  generateRandomNum();
 
 }
 
 //Generate random number from 1-100
 function generateRandomNum() {
-
-}
-//Giving feedback for each guess - too hot, too cold - Should appear in div#feedback
-function giveFeedback() {
-
+  var number = Math.floor((Math.random() * 100) + 1); 
+  return number;
 }
 
-//Get input from user and verify it is a number
-function getUserInput() {	
-	var userGuess = parseInt($("#userGuess").val());
-	if(isNaN(userGuess)) {
-		$("#userGuess").val("");
-		alert("Please enter a number.");
-	} else {
-		return userGuess;
-	}
+// 
+function guessList(user) {
+  // add to the list of numbers
+  $('#guessList').append('<li>' + user + '</li>');
 }
+
+
 
 
